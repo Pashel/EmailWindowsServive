b03b28389baf91bf976ada7aa60f444326f32585
pashel <90pashel90@gmail.com>
2017-11-12 23:51:39 +0300

    Add project files.

---
+++ WindowsServiceTask/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsServiceTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
